f9adfe05bf0def12a51bb513c820cad948aa9acc
Toprak Nihat Deniz Ozturk <45317988+toprakdeniz@users.noreply.github.com>
2020-06-14 17:51:21 +0300

    Update tvshutdown.sh

--- tvshutdown.sh
+++ tvshutdown.sh
@@ -1,3 +1,6 @@
+# Script turns off tv before the device
+# cec-client application is required
+# add this script to /usr/bin to directly call: $bash tvshutdown.sh
+# You might need to udate adress!
 echo 'standby 0.0.0.0' | cec-client -s -d 1
 shutdown now
-
